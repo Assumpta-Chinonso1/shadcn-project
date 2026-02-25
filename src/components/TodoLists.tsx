f71db137006bfcdb2b5d52873ed5d192abdbb8d8
Celestine Assumpta <assumptachinonso42@gmail.com>
2026-02-25 18:53:27 +0100

    ShadCN Calendar and Popover

--- src/components/TodoLists.tsx
+++ src/components/TodoLists.tsx
@@ -1,11 +1,42 @@
+"use client"
+
+import { useState } from "react"
+import { Button } from "./ui/button"
 import { Card } from "./ui/card"
 import { Checkbox } from "./ui/checkbox"
+import { Popover, PopoverContent, PopoverDescription, PopoverHeader, PopoverTitle, PopoverTrigger } from "./ui/popover"
 import { ScrollArea } from "./ui/scroll-area"
+import {  Calendar1Icon } from "lucide-react"
+import { format } from "date-fns"
+import { Calendar } from "./ui/calendar"
 
 const TodoLists = () => {
+    const [date, setDate] = useState<Date | undefined>(new Date())
+    const [popUp, setPopUp] = useState(false)
+
     return(
         <div className="">
-            Calender
+            <h1 className="text-lg font-medium mb-6">Todo List</h1>
+            <Popover open={popUp} onOpenChange={setPopUp}>
+              <PopoverTrigger asChild >
+                   <Button className="w-full">
+                    <Calendar1Icon/>
+                    {date ? format(date, "PPP") : <span>Pick a date</span> }
+                   </Button>
+               </PopoverTrigger>
+
+                 <PopoverContent className="p-0 w-auto">
+                       <Calendar
+                     mode="single"
+                      selected={date}
+                       onSelect={(date) => {
+                        setDate(date);
+                        setPopUp(false);
+                       }}
+                    
+                                  />
+                </PopoverContent>
+                </Popover>
             {/*List*/}
             <ScrollArea className="max-h-[400px] mt-4 overflow-y-auto no-scrollbar">
                 <div className="flex flex-col gap-4">
@@ -13,10 +44,8 @@ const TodoLists = () => {
                 <Card className="p-4">
                     <div className="flex items-center gap-4">
                         <Checkbox id="item1" />
-                        <label htmlFor="item1" className="text-sm text-muted-foreground">
-                            Lorem ipsum dolor sit amet consectetu.
-                             adipisicing elit. 
-                            Eaque voluptates assumenda, 
+                        <label htmlFor="item1" className="text-2xl text-muted-foreground">
+                            Code
                            
                         </label>
                     </div>
@@ -24,10 +53,8 @@ const TodoLists = () => {
                  <Card className="p-4">
                     <div className="flex items-center gap-4">
                         <Checkbox id="item1" />
-                        <label htmlFor="item1" className="text-sm text-muted-foreground">
-                            Lorem ipsum dolor sit amet consectetu.
-                             adipisicing elit. 
-                            Eaque voluptates assumenda, 
+                        <label htmlFor="item1" className="text-2xl text-muted-foreground">
+                         Gym
                            
                         </label>
                     </div>
@@ -35,10 +62,8 @@ const TodoLists = () => {
                  <Card className="p-4">
                     <div className="flex items-center gap-4">
                         <Checkbox id="item1" />
-                        <label htmlFor="item1" className="text-sm text-muted-foreground">
-                            Lorem ipsum dolor sit amet consectetu.
-                             adipisicing elit. 
-                            Eaque voluptates assumenda, 
+                        <label htmlFor="item1" className="text-2xl text-muted-foreground">
+                           Pray
                            
                         </label>
                     </div>
@@ -46,10 +71,8 @@ const TodoLists = () => {
                  <Card className="p-4">
                     <div className="flex items-center gap-4">
                         <Checkbox id="item1" />
-                        <label htmlFor="item1" className="text-sm text-muted-foreground">
-                            Lorem ipsum dolor sit amet consectetu.
-                             adipisicing elit. 
-                            Eaque voluptates assumenda, 
+                        <label htmlFor="item1" className="text-2xl text-muted-foreground">
+                            Read a book
                            
                         </label>
                     </div>
@@ -57,10 +80,16 @@ const TodoLists = () => {
                  <Card className="p-4">
                     <div className="flex items-center gap-4">
                         <Checkbox id="item1" />
-                        <label htmlFor="item1" className="text-sm text-muted-foreground">
-                            Lorem ipsum dolor sit amet consectetu.
-                             adipisicing elit. 
-                            Eaque voluptates assumenda, 
+                        <label htmlFor="item1" className="text-2xl text-muted-foreground">
+                            Write a blog post
+                        </label>
+                    </div>
+                </Card>
+                 <Card className="p-4">
+                    <div className="flex items-center gap-4">
+                        <Checkbox id="item1" />
+                        <label htmlFor="item1" className="text-2xl text-muted-foreground">
+                            Cook dinner
                            
                         </label>
                     </div>
@@ -68,10 +97,8 @@ const TodoLists = () => {
                  <Card className="p-4">
                     <div className="flex items-center gap-4">
                         <Checkbox id="item1" />
-                        <label htmlFor="item1" className="text-sm text-muted-foreground">
-                            Lorem ipsum dolor sit amet consectetu.
-                             adipisicing elit. 
-                            Eaque voluptates assumenda, 
+                        <label htmlFor="item1" className="text-2xl text-muted-foreground">
+                            Go for a walk
                            
                         </label>
                     </div>
@@ -79,10 +106,8 @@ const TodoLists = () => {
                  <Card className="p-4">
                     <div className="flex items-center gap-4">
                         <Checkbox id="item1" />
-                        <label htmlFor="item1" className="text-sm text-muted-foreground">
-                            Lorem ipsum dolor sit amet consectetu.
-                             adipisicing elit. 
-                            Eaque voluptates assumenda, 
+                        <label htmlFor="item1" className="text-2xl text-muted-foreground">
+                            Call a friend
                            
                         </label>
                     </div>
@@ -90,10 +115,8 @@ const TodoLists = () => {
                  <Card className="p-4">
                     <div className="flex items-center gap-4">
                         <Checkbox id="item1" />
-                        <label htmlFor="item1" className="text-sm text-muted-foreground">
-                            Lorem ipsum dolor sit amet consectetu.
-                             adipisicing elit. 
-                            Eaque voluptates assumenda, 
+                        <label htmlFor="item1" className="text-2xl text-muted-foreground">
+                            Plan a trip
                            
                         </label>
                     </div>
@@ -101,21 +124,8 @@ const TodoLists = () => {
                  <Card className="p-4">
                     <div className="flex items-center gap-4">
                         <Checkbox id="item1" />
-                        <label htmlFor="item1" className="text-sm text-muted-foreground">
-                            Lorem ipsum dolor sit amet consectetu.
-                             adipisicing elit. 
-                            Eaque voluptates assumenda, 
-                           
-                        </label>
-                    </div>
-                </Card>
-                 <Card className="p-4">
-                    <div className="flex items-center gap-4">
-                        <Checkbox id="item1" />
-                        <label htmlFor="item1" className="text-sm text-muted-foreground">
-                            Lorem ipsum dolor sit amet consectetu.
-                             adipisicing elit. 
-                            Eaque voluptates assumenda, 
+                        <label htmlFor="item1" className="text-2xl text-muted-foreground">
+                            Learn a new skill
                            
                         </label>
                     </div>
